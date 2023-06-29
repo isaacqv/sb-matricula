f5413131dba853f469adfe1f70f4e4f05fe2acba
isaacqv <isaacqv@gmail.com>
2023-06-28 20:07:54 -0500

    Sesion13||Implementacion de Logger

--- src/main/java/edu/cibertec/matricula/controller/CursoController.java
+++ src/main/java/edu/cibertec/matricula/controller/CursoController.java
@@ -3,6 +3,10 @@ package edu.cibertec.matricula.controller;
 import edu.cibertec.matricula.dao.entity.CursoEntity;
 import edu.cibertec.matricula.service.CursoService;
 import javax.validation.Valid;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -12,7 +16,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class CursoController {
+
+    //Logger logger = LoggerFactory.getLogger(CursoController.class);
 
     @Autowired
     private CursoService cursoService;
@@ -28,9 +35,11 @@ public class CursoController {
     public ModelAndView cursoGrabar(@Valid @ModelAttribute("cursoBean") CursoEntity ce,
                                     BindingResult valida,
                                     @RequestParam(value = "accion", required = false) String accion) {
+        log.debug("Antes de insertar un curso");
         ModelAndView mv = new ModelAndView("curso");
         System.out.println(valida.toString());
         if (valida.hasErrors()) {
+            log.warn("Falló el insertar un curso");
             mv.addObject("lista", cursoService.listarTodos());
             mv.addObject("cursoBean", ce);
         } else {
@@ -42,13 +51,16 @@ public class CursoController {
             //cursoService.insertar(ce);
             mv.addObject("lista", cursoService.listarTodos());
             mv.addObject("cursoBean", new CursoEntity());
+            log.warn("Grabó el curso! "+ce.getNomCurso());
         }
         return mv;
     }
 
     @RequestMapping("cursoEliminar")
     public ModelAndView cursoEliminar(@RequestParam("codigo") int codigo) {
+        log.info("Antes de eliminar un curso");
         cursoService.eliminar(codigo);
+        log.warn("Despues de eliminar un curso "+codigo);
         return new ModelAndView("redirect:cursoListar");
     }
     @RequestMapping("cursoModificar")
